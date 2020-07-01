eaf8d256b98f685541982b5b75fe1fae7de4a35e
CyrilLepretre <cyril.lepretre@axa.fr>
2020-07-01 23:00:00 +0200

    Log cleaning

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ app.post('/contact/authent', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
 		// watch for any connect issues
 		if (err) console.log(err);
-		console.log('req.body : ' + JSON.stringify(req.body));
-		console.log('req.body.user:-' + req.body.user + '-');
-		console.log('req.body.pwd:-' + req.body.pwd + '-');
 		conn.query(
 			'SELECT Id, FirstName, LastName, Email, Phone, SfId FROM salesforce.Contact WHERE LOWER(Email) = LOWER($1) AND MobileAppPwd__c = ($2)',
 			[req.body.user.trim(), req.body.pwd.trim()],
@@ -42,7 +39,6 @@ app.post('/contracts', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
 		// watch for any connect issues
 		if (err) console.log(err);
-		console.log('req.body : ' + JSON.stringify(req.body));
 		conn.query(
 			'SELECT Id, Name, Product_Assu__c FROM salesforce.Contract_Assu__c WHERE LOWER(Contact__c) = LOWER($1)',
 			[req.body.contactId.trim()],
@@ -58,33 +54,21 @@ app.post('/contracts', function(req, res) {
 				else {
 					// Results found => sub query to retrieve product of eachr row with the SfId
 					result.rows.forEach(function(item, index) {
-						console.log('CLE : row ' + index + ', item ' + JSON.stringify(item));
-						console.log('CLE : item.product_assu__c to modify : -' + item.product_assu__c + '-');
 						conn.query(
 							'SELECT Name FROM salesforce.Product_Assu__c WHERE SfId = $1',
 							[item.product_assu__c],
 							function(err2, result2) {
 								done();
-								console.log('CLE result2:'+JSON.stringify(result2));
-								console.log('CLE err2:'+JSON.stringify(err2));
 								if (err2 == null && result2.rowCount != 0) {
 									// Result found in Produc_Assu__c => replace product Id by product name in result
 									item.product_assu__c = result2.rows[0].name;
-									console.log('CLE - Update name new : ' + result2.rows[0].name);
 								}
-								console.log('CLE --------------- length ' + result.rows.length);
-								console.log('CLE --------------- index + 1 ' + (index + 1));
 								if (result.rows.length == index + 1) {
 									callbackContracts(res, result);
 								}
 							}
 						);
-					/*}).then(function() {
-						console.log('CLE : RESULT =' +JSON.stringify(result));
-						res.json(result);*/
 					});
-					//console.log('CLE : RESULT =' +JSON.stringify(result));
-					//res.json(result);
 				}
 			}
 		);
